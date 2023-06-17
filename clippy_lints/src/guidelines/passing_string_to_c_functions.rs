c091b77fb61797c78c5e365530b3b0d8368ed787
J-ZhengLi <lizheng135@huawei.com>
2023-06-17 15:59:56 +0800

    add configuration for lint [`mem_unsafe_functions`]; finish implement lint [`mem_unsafe_functions`]; add simple description for unimplemented lints;
    Signed-off-by: J-ZhengLi <lizheng135@huawei.com>

--- clippy_lints/src/guidelines/passing_string_to_c_functions.rs
+++ clippy_lints/src/guidelines/passing_string_to_c_functions.rs
@@ -1,8 +1,1 @@
-use rustc_lint::{LateContext, LintContext};
 
-use super::PASSING_STRING_TO_C_FUNCTIONS;
-
-// TODO: Adjust the parameters as necessary
-pub(super) fn check(cx: &LateContext<'_>) {
-    todo!();
-}
--- clippy_lints/src/lib.rs
+++ clippy_lints/src/lib.rs
@@ -960,7 +960,8 @@ pub fn register_plugins(store: &mut rustc_lint::LintStore, sess: &Session, conf:
     store.register_late_pass(|_| Box::new(tests_outside_test_module::TestsOutsideTestModule));
     store.register_late_pass(|_| Box::new(manual_slice_size_calculation::ManualSliceSizeCalculation));
     store.register_early_pass(|| Box::new(suspicious_doc_comments::SuspiciousDocComments));
-    store.register_late_pass(|_| Box::new(guidelines::GuidelineLints));
+    let mem_unsafe_functions = conf.mem_unsafe_functions.clone();
+    store.register_late_pass(move |_| Box::new(guidelines::GuidelineLints::new(mem_unsafe_functions.clone())));
     // add lints here, do not remove this comment, it's used in `new_lint`
 }
 
