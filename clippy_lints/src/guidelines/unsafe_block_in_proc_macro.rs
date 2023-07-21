e3d26a5110a5ecd8dcfe5e5eebd9e2cab2a9e180
J-ZhengLi <lizheng135@huawei.com>
2023-07-21 18:19:09 +0800

    hopefully fixing [`unsafe_block_in_proc_macro`] FP, with the lack of ui test
    Signed-off-by: J-ZhengLi <lizheng135@huawei.com>

--- clippy_lints/src/guidelines/unsafe_block_in_proc_macro.rs
+++ clippy_lints/src/guidelines/unsafe_block_in_proc_macro.rs
@@ -2,11 +2,16 @@ use clippy_utils::diagnostics::span_lint_and_help;
 use clippy_utils::source::snippet_opt;
 use if_chain::if_chain;
 use rustc_data_structures::fx::FxHashSet;
-use rustc_hir::Expr;
+use rustc_hir::{Expr, Item, ItemKind, OwnerNode};
 use rustc_lint::LateContext;
 use rustc_span::hygiene::{ExpnKind, MacroKind};
-use rustc_span::{sym, Span};
+use rustc_span::{sym, symbol::Ident, Span};
 
+// FIXME: Currently, this lint does not have a working ui test,
+// because it needs to be tested with `#[proc_macro]` attribute,
+// but I couldn't find information about how to write such ui test in Clippy.
+// So, right now the only solution was to compile first, then test in real code,
+// which is a big headache.
 use super::UNSAFE_BLOCK_IN_PROC_MACRO;
 
 pub(super) fn check(cx: &LateContext<'_>, expr: &Expr<'_>, call_sites: &mut FxHashSet<Span>) {
@@ -44,9 +49,26 @@ fn contains_unsafe_block(code: &str) -> bool {
 }
 
 fn emit_lint_message(cx: &LateContext<'_>, expr: &Expr<'_>, call_site: Span, could_be_fn: bool) {
-    let parent_hid = cx.tcx.hir().parent_id(expr.hir_id);
-    let name = cx.tcx.item_name(parent_hid.owner.to_def_id());
-    let msg = &format!("procedural macro `{name}` may hiding unsafe operations");
+    let hir_map = cx.tcx.hir();
+    let mut owner_ident: Option<Ident> = None;
+    for (hid, node) in hir_map.parent_owner_iter(expr.hir_id) {
+        if let OwnerNode::Item(Item {
+            kind: ItemKind::Fn(..), ..
+        }) = node
+        {
+            let attrs = hir_map.attrs(hid.into());
+            if attrs
+                .iter()
+                .any(|attr| matches!(attr.ident(), Some(ident) if ident.name == sym::proc_macro))
+            {
+                owner_ident = node.ident();
+                break;
+            }
+        }
+    }
+    let Some(ident) = owner_ident else { return };
+    let name = ident.name.as_str();
+    let msg = &format!("function-like procedural macro `{name}` could masking unsafe operations");
     let extra_sugg = if could_be_fn {
         "if the block was in a function, \
         put an `unsafe` keyword on its definition: `unsafe fn ...`, \n\
