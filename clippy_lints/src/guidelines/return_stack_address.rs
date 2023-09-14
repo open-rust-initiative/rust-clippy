9404785ebe1dafb44892e6d4e294a74b3c06aa4b
J-ZhengLi <lizheng135@huawei.com>
2023-09-14 21:13:06 +0800

    fix FP and ICE error
    Signed-off-by: J-ZhengLi <lizheng135@huawei.com>

--- clippy_lints/src/guidelines/return_stack_address.rs
+++ clippy_lints/src/guidelines/return_stack_address.rs
@@ -1,13 +1,12 @@
-use clippy_utils::diagnostics::span_lint_and_then;
+use clippy_utils::diagnostics::span_lint_and_note;
 use clippy_utils::{path_res, peel_hir_expr_refs};
 use rustc_data_structures::fx::FxHashMap;
 use rustc_hir::def::Res;
 use rustc_hir::hir_id::{HirId, HirIdSet};
 use rustc_hir::intravisit::{walk_block, walk_expr, Visitor};
-use rustc_hir::{Block, Expr, ExprKind, Local, Ty, TyKind};
-use rustc_hir_analysis::hir_ty_to_ty;
+use rustc_hir::{Block, Expr, ExprKind, Local, Path, QPath, Ty, TyKind};
 use rustc_lint::LateContext;
-use rustc_middle::ty::TyCtxt;
+use rustc_span::symbol::sym;
 use rustc_span::Span;
 
 use super::peel_casts;
@@ -22,7 +21,6 @@ pub(super) fn check<'tcx>(cx: &LateContext<'tcx>, block: &'tcx Block<'tcx>, visi
         local: FxHashMap::default(),
         rets: Vec::new(),
         visited_blocks,
-        tcx: cx.tcx,
     };
     collector.visit_block(block);
 
@@ -37,13 +35,17 @@ pub(super) fn check<'tcx>(cx: &LateContext<'tcx>, block: &'tcx Block<'tcx>, visi
             ) => {
                 let castee = peel_hir_expr_refs(peel_casts(cast_expr)).0;
                 let maybe_path = peel_method_calls(castee);
-                if let Some(decl_span) = get_value_from_local_set(cx, maybe_path, &collector.local) {
+                if let Some(decl_span) = get_simple_local_ty_span(cx, maybe_path, &collector.local) {
                     emit_lint(cx, ret.span, decl_span);
                 }
             },
-            ExprKind::MethodCall(_, caller, ..) => {
+            // FIXME: The [`sym::as_mut_ptr`] wasn't available in this toolchain yet,
+            // change the str comparison to `callee.ident.name == sym::as_mut_ptr` later.
+            ExprKind::MethodCall(methond_name, caller, ..)
+                if methond_name.ident.name == sym::as_ptr || methond_name.ident.as_str() == "as_mut_ptr" =>
+            {
                 let maybe_path = peel_method_calls(caller);
-                if let Some(decl_span) = get_value_from_local_set(cx, maybe_path, &collector.local) {
+                if let Some(decl_span) = get_simple_local_ty_span(cx, maybe_path, &collector.local) {
                     emit_lint(cx, ret.span, decl_span);
                 }
             },
@@ -52,9 +54,12 @@ pub(super) fn check<'tcx>(cx: &LateContext<'tcx>, block: &'tcx Block<'tcx>, visi
     }
 }
 
-fn get_value_from_local_set(cx: &LateContext<'_>, maybe_path: &Expr<'_>, set: &FxHashMap<HirId, Span>) -> Option<Span> {
-    if let Res::Local(hir_id) = path_res(cx, maybe_path) {
-        set.get(&hir_id).copied()
+fn get_simple_local_ty_span(cx: &LateContext<'_>, maybe_path: &Expr<'_>, set: &FxHashMap<HirId, Span>) -> Option<Span> {
+    if let Res::Local(hir_id) = path_res(cx, maybe_path) &&
+        let Some(span) = set.get(&hir_id) &&
+        cx.typeck_results().node_type(hir_id).is_simple_ty()
+    {
+        Some(*span)
     } else {
         None
     }
@@ -72,21 +77,21 @@ struct LocalAndRetCollector<'a, 'tcx> {
     local: FxHashMap<HirId, Span>,
     rets: Vec<&'a Expr<'tcx>>,
     visited_blocks: &'a mut HirIdSet,
-    tcx: TyCtxt<'tcx>,
 }
 
 impl<'a, 'v> Visitor<'v> for LocalAndRetCollector<'a, 'v> {
     fn visit_local(&mut self, local: &'v Local<'v>) {
-        if matches!(
-            local.init,
-            Some(
-                Expr {
-                    kind: ExprKind::Lit(_),
+        if local.ty.is_some()
+            || matches!(
+                local.init,
+                Some(
+                    Expr {
+                        kind: ExprKind::Lit(_),
+                        ..
+                    },
                     ..
-                },
-                ..
+                )
             )
-        ) || matches!(local.ty, Some(ty) if hir_ty_to_ty(self.tcx, ty).is_simple_ty())
         {
             self.local.insert(local.pat.hir_id, local.span);
         }
@@ -94,7 +99,16 @@ impl<'a, 'v> Visitor<'v> for LocalAndRetCollector<'a, 'v> {
     fn visit_expr(&mut self, expr: &'v Expr<'v>) {
         match &expr.kind {
             ExprKind::Assign(assign_to, assigning, _) if matches!(assigning.kind, ExprKind::Lit(_)) => {
-                self.local.insert(assign_to.hir_id, expr.span);
+                if let ExprKind::Path(QPath::Resolved(
+                    _,
+                    Path {
+                        res: Res::Local(hir_id),
+                        ..
+                    },
+                )) = &assign_to.kind
+                {
+                    self.local.insert(*hir_id, expr.span);
+                }
             },
             ExprKind::Ret(Some(ret_expr)) => {
                 self.rets.push(ret_expr);
@@ -112,14 +126,12 @@ impl<'a, 'v> Visitor<'v> for LocalAndRetCollector<'a, 'v> {
 }
 
 fn emit_lint(cx: &LateContext<'_>, span: Span, decl_span: Span) {
-    span_lint_and_then(
+    span_lint_and_note(
         cx,
         RETURN_STACK_ADDRESS,
         span,
         "returning a pointer to stack address",
-        |diag| {
-            diag.span_note(decl_span, "local variable declared/assigned here");
-            diag.help("consider declaring it as const or static variable");
-        },
+        Some(decl_span),
+        "local variable declared/assigned here",
     );
 }
--- tests/ui/guidelines/return_stack_address.rs
+++ tests/ui/guidelines/return_stack_address.rs
@@ -1,12 +1,46 @@
 #![allow(unused)]
 #![allow(clippy::needless_return)]
 #![allow(clippy::redundant_clone)]
+#![allow(clippy::needless_late_init)]
 #![warn(clippy::return_stack_address)]
+
+#[derive(Clone)]
+struct NonCopiable(Vec<u8>);
+
+impl NonCopiable {
+    fn as_ptr(&self) -> *const u8 {
+        1_u8 as *const u8
+    }
+}
+
+#[derive(Clone, Copy)]
+struct Copiable(u8);
+
+impl Copiable {
+    fn as_ptr(&self) -> *const u8 {
+        self.0 as *const _
+    }
+}
+
+trait ToPtr {
+    fn to_ptr(self) -> *const u8;
+}
+
+impl ToPtr for u8 {
+    fn to_ptr(self) -> *const u8 {
+        1 as *const u8
+    }
+}
 
 fn fn_ret() {
     fn implicit_int() -> *const i32 {
         let val: i32 = 100;
         &val as *const _ // lint
+    }
+
+    fn implicit_int_mut() -> *mut i32 {
+        let mut val: i32 = 100;
+        &mut val as *mut _ // lint
     }
 
     fn explicit_int() -> *const i32 {
@@ -46,6 +80,32 @@ fn fn_ret() {
     fn complicated() -> *const core::ffi::c_char {
         let val = "123";
         val.to_string().clone().as_ptr() as *const i8 as *const i32 as *const _ // lint
+    }
+
+    fn non_simple_ty() -> *const u8 {
+        let val = String::from("123");
+        val.as_ptr() // don't lint
+    }
+
+    fn declare_then_assign() -> *const i32 {
+        let val;
+        val = 12;
+        &val as *const _ // lint
+    }
+
+    fn ret_non_copiable() -> *const u8 {
+        let val: NonCopiable = NonCopiable(vec![1, 2, 3]);
+        val.as_ptr() // don't lint
+    }
+
+    fn ret_copiable() -> *const u8 {
+        let val: Copiable = Copiable(123);
+        val.as_ptr() // Maybe FN?
+    }
+
+    fn ret_other_call() -> *const u8 {
+        let val: u8 = 20;
+        val.to_ptr() // don't lint
     }
 }
 
